764ebed37ef3a61ec3f6ba79992ffda348ab11cf
Adrien Ruault <ar@visium.ch>
2023-04-20 11:11:19 +0200

    setting up docker-compose

--- dvc_pipeline/src/pipeline/download_data/downloading_data.py
+++ dvc_pipeline/src/pipeline/download_data/downloading_data.py
@@ -1,11 +1,13 @@
 from pathlib import Path
 import typer
 from sklearn import datasets
+import os
 
 
 def main(output_dir: Path = typer.Option(...)):
+    os.makedirs(output_dir, exist_ok=True)
     iris = datasets.load_iris(as_frame=True)
-    iris.to_parquet(output_dir / "iris.parquet")
+    iris['data'].to_parquet(output_dir / "iris.parquet")
 
 
 if __name__ == "__main__":
